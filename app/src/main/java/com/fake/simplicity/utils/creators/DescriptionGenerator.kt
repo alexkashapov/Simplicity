50b617b5c1fa4d7884e24c7f38b56f106d58ce3f
Alex Kashapov <alexander.kashapov@magenta-technology.com>
2020-11-03 11:27:04 +0400

    Add elements types in item

--- app/src/main/java/com/fake/simplicity/utils/creators/DescriptionGenerator.kt
+++ app/src/main/java/com/fake/simplicity/utils/creators/DescriptionGenerator.kt
@@ -4,7 +4,7 @@ import kotlin.reflect.full.declaredMemberProperties
 
 object DescriptionGenerator {
     fun generateDescription(item: Any): String {
-        val result = arrayListOf<String>()
+        val result = arrayListOf("${item::class.simpleName}")
         item::class.declaredMemberProperties.forEach { member ->
             val text = "<b>${member.name}</b> ${member.call(item)}"
             result.add(text)
